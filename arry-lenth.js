64474afc222c1df1b162a359656b819fd841c60c
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-13 19:30:38 +0800

    first commit

---
+++ arry-lenth.js
@@ -0,0 +1,9 @@
+// for marks uses and lendth use to added more marks 
+
+var marks = [13, 15, 14, 20, 18];
+for (var i = 0; i < marks.length; i++){
+    if (marks[i] >= 15) {
+        continue;
+    }
+    console.log(marks[i]);
+}
